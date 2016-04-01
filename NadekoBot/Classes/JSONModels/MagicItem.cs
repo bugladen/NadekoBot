e9d79af4ce0d1e8f781e05891197f03e660dab37
appelemac <appelemac@gmail.com>
2016-04-01 19:41:25 +0200

    Added ~magicitem

---
+++ NadekoBot/Classes/JSONModels/MagicItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Classes.JSONModels
+{
+    class MagicItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public override string ToString() =>
+            $"✨`{Name}`\n\t*{Description}*";
+    }
+}
