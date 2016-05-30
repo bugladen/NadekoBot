5dda8d263e46424f20276d0d7a5cff762b427eff
Master Kwoth <srki0509@gmail.com>
2016-05-30 19:48:37 +0200

    unneeded isnullorempty

--- NadekoBot/Modules/Searches/Commands/IMDB/ImdbScraper.cs
+++ NadekoBot/Modules/Searches/Commands/IMDB/ImdbScraper.cs
@@ -45,11 +45,7 @@ namespace NadekoBot.Modules.Searches.Commands.IMDB
             ImdbMovie mov = new ImdbMovie();
             string imdbUrl = "http://www.imdb.com/title/" + imdbId + "/";
             mov.Status = false;
-            if (!string.IsNullOrEmpty(imdbUrl))
-            {
-                ParseIMDbPage(imdbUrl, GetExtraInfo, mov);
-            }
-
+            ParseIMDbPage(imdbUrl, GetExtraInfo, mov);
             return mov;
         }
 
