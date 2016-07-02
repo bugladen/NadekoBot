1b3b65817e923c14eb57e87964e72466c611e664
Master Kwoth <srki0509@gmail.com>
2016-07-02 22:42:45 +0200

    drawing 5 cards now mentions the person who invoked the command, thx Pajdo Warrior

--- NadekoBot/Modules/Gambling/DrawCommand.cs
+++ NadekoBot/Modules/Gambling/DrawCommand.cs
@@ -79,7 +79,7 @@ namespace NadekoBot.Modules.Gambling
                 await e.Channel.SendFile(images.Count + " cards.jpg", bitmap.ToStream()).ConfigureAwait(false);
                 if (cardObjects.Count == 5)
                 {
-                    await e.Channel.SendMessage(Cards.GetHandValue(cardObjects)).ConfigureAwait(false);
+                    await e.Channel.SendMessage($"{e.User.Mention} `{Cards.GetHandValue(cardObjects)}`").ConfigureAwait(false);
                 }
             }
             catch (Exception ex)
