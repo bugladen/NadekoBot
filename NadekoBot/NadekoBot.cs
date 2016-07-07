f0c85eaafc4930b38c453026112b570ac00d4f32
Master Kwoth <srki0509@gmail.com>
2016-07-07 04:46:33 +0200

    fixed customreactions always loading default reactions, even when they are deleted #370

--- NadekoBot/NadekoBot.cs
+++ NadekoBot/NadekoBot.cs
@@ -196,7 +196,7 @@ namespace NadekoBot
                     return;
                 }
 #if NADEKO_RELEASE
-                await Task.Delay(100000).ConfigureAwait(false);
+                await Task.Delay(120000).ConfigureAwait(false);
 #else
                 await Task.Delay(1000).ConfigureAwait(false);
 #endif
