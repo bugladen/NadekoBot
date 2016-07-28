033acead6088ebdc6431ee8db3cfb41c8696fa1c
blitz4694 <reapercon_90@hotmail.com>
2016-07-29 06:31:40 +1000

    small mistake

--- NadekoBot/Modules/Administration/AdministrationModule.cs
+++ NadekoBot/Modules/Administration/AdministrationModule.cs
@@ -628,7 +628,7 @@ namespace NadekoBot.Modules.Administration
                 cgb.CreateCommand(Prefix + "prune")
                     .Alias(Prefix + "clr")
                     .Description(
-                    "`.prune` removes all nadeko's messages in the last 100 messages.`.prune X` removes last X messages from the channel (up to 100)`.prune @Someone` removes all Someone's messages in the last 100 messages.`.prune @Someone X` removes last X 'Someone's' messages in the channel. "+
+                    ".prune removes all nadeko's messages in the last 100 messages., .prune X removes last X messages from the channel (up to 100), .prune @Someone removes all Someone's messages in the last 100 messages., .prune @Someone X removes last X 'Someone's messages in the channel. "+
                     $"| `{Prefix}prune` or `{Prefix}prune 5` or `{Prefix}prune @Someone` or `{Prefix}prune @Someone X`")
                     .Parameter("user_or_num", ParameterType.Optional)
                     .Parameter("num", ParameterType.Optional)
