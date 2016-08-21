09ff6aefb7988b45f74e673dbe84c1ecf2373887
Kwoth <srki0509@gmail.com>
2016-08-21 04:34:26 +0200

    Mentionrole is now mentioning once again

--- src/NadekoBot/Modules/Administration/AdministrationModule.cs
+++ src/NadekoBot/Modules/Administration/AdministrationModule.cs
@@ -646,7 +646,7 @@ namespace NadekoBot.Modules.Administration
             foreach (var role in roles)
             { 
                 send += $"\n`{role.Name}`\n";
-                send += string.Join(", ", (await channel.Guild.GetUsersAsync()).Where(u => u.Roles.Contains(role)).Distinct());
+                send += string.Join(", ", (await channel.Guild.GetUsersAsync()).Where(u => u.Roles.Contains(role)).Distinct().Select(u=>u.Mention));
             }
 
             while (send.Length > 2000)
