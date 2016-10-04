21aabcc1146c4296a27ad346baeb25ca8b4766f1
Nitix <guillaume.pierson.gp@gmail.com>
2016-10-05 01:57:35 +0200

    Add migration command

---
+++ src/NadekoBot/Migrations/20161004233426_MigrationVersion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NadekoBot.Migrations
+{
+    public partial class MigrationVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MigrationVersion",
+                table: "BotConfig",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MigrationVersion",
+                table: "BotConfig");
+        }
+    }
+}
---
+++ src/NadekoBot/Migrations/20161004233637_HelpString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NadekoBot.Migrations
+{
+    public partial class HelpString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HelpString",
+                table: "BotConfig",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HelpString",
+                table: "BotConfig");
+        }
+    }
+}
