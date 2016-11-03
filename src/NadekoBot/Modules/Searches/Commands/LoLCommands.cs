83e0ccd226969d9a58b741a6cc6b94ba66bfaf6a
Kwoth <srki0509@gmail.com>
2016-11-03 13:04:15 +0100

    lolcommands have banrate percentage now

--- src/NadekoBot/Modules/Searches/Commands/LoLCommands.cs
+++ src/NadekoBot/Modules/Searches/Commands/LoLCommands.cs
@@ -58,7 +58,7 @@ namespace NadekoBot.Modules.Searches
                     {
                         if (i % 2 == 0 && i != 0)
                             sb.AppendLine();
-                        sb.Append($"`{i + 1}.` **{dataList[i]["name"]}**  ");
+                        sb.Append($"`{i + 1}.` **{dataList[i]["name"]}** {dataList[i]["general"]["banRate"]}% ");
                         //sb.AppendLine($" ({dataList[i]["general"]["banRate"]}%)");
                     }
 
