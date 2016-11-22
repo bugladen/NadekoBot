f04ce0c3172f7460f7025549b65dcfc6866b07e5
samvaio <samdivaio@icloud.com>
2016-11-23 00:18:38 +0530

    Update LogCommand.cs

--- src/NadekoBot/Modules/Administration/Commands/LogCommand.cs
+++ src/NadekoBot/Modules/Administration/Commands/LogCommand.cs
@@ -409,7 +409,7 @@ namespace NadekoBot.Modules.Administration
                 {
                     try
                     {
-                        var str = $@"🕔`{prettyCurrentTime}`\t👤`{msg.Author.Username}#{msg.Author.Discriminator}`\t**Deleted Message**\t#⃣ `{channel.Name}`
+                        var str = $@"🕔`{prettyCurrentTime}`👤`{msg.Author.Username}#{msg.Author.Discriminator}` **Deleted Message** #⃣ `{channel.Name}`
 🗑 `{msg.Resolve(userHandling: UserMentionHandling.NameAndDiscriminator)}`";
                         if (msg.Attachments.Any())
                             str += $"{Environment.NewLine}`Attachements`: {string.Join(", ", msg.Attachments.Select(a => a.ProxyUrl))}";
