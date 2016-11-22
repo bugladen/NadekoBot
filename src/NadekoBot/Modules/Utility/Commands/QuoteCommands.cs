84c811bcd1141a5c9e209535c6a54e323fc81fea
samvaio <samdivaio@icloud.com>
2016-11-23 04:36:35 +0530

    Update QuoteCommands.cs

--- src/NadekoBot/Modules/Utility/Commands/QuoteCommands.cs
+++ src/NadekoBot/Modules/Utility/Commands/QuoteCommands.cs
@@ -52,7 +52,7 @@ namespace NadekoBot.Modules.Utility
             using (var uow = DbHandler.Instance.GetUnitOfWork())
             {
 //                quote = await uow.Quotes.GetRandomQuoteByKeywordAsync(channel.Guild.Id, keyword).ConfigureAwait(false);
-                quote = await uow.Quotes.GetRandomQuoteByKeywordAsync(keyword, keyword).ConfigureAwait(false);
+                quote = await uow.Quotes.GetRandomQuoteByKeywordAsync(channel.Id, keyword).ConfigureAwait(false);
             }
 
             if (quote == null)
