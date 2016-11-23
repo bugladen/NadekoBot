0d3c61628402181d1c7fa3d3587a7ed0622d5c31
samvaio <samdivaio@icloud.com>
2016-11-23 16:36:26 +0530

    Update LogCommand.cs

--- src/NadekoBot/Modules/Administration/Commands/LogCommand.cs
+++ src/NadekoBot/Modules/Administration/Commands/LogCommand.cs
@@ -143,7 +143,7 @@ namespace NadekoBot.Modules.Administration
                             else if (before.Roles.Count() > after.Roles.Count())
                             {
                                 var diffRoles = before.Roles.Where(r => !after.Roles.Contains(r)).Select(r => "`" + r.Name + "`");
-                                str += $"👤`{before.ToString()}` **User's Role Removed** 🆔 `{before.Id}`\n⚔⏬ {string.Join(", ", diffRoles)}, **Remaining:** `{string.Join(", ", before.Roles.Select(r => r.Name)).SanitizeMentions()}`";
+                                str += $"👤`{before.ToString()}` **User's Role Removed** 🆔 `{before.Id}`\n⚔⏬ {string.Join(", ", diffRoles)}, **Remaining:** `{string.Join(", ", after.Roles.Select(r => r.Name)).SanitizeMentions()}`";
                             }
                         }
                         else
