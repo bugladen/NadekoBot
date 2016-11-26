ba5afa8da05de043ee6f16eaa4d66dd7e74e0d47
Pg <pg.developper.fr@gmail.com>
2016-11-26 03:12:20 +0100

    Add getter and setter for module

---
+++ src/NadekoBot/Services/Database/Models/PokemonType.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Services.Database.Models
+{
+    class UserPokeTypes : DbEntity
+    {
+        public long UserId { get; set; }
+        public string type { get; set; }
+    }
+}
