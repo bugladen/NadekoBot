96cecee7fffa1e4dfd5fb1adbba4e9e1f6cbe105
samvaio <samdivaio@icloud.com>
2016-11-30 17:34:30 +0530

    Update Stable.bat

--- scripts/Stable.bat
+++ scripts/Stable.bat
@@ -20,7 +20,7 @@ CD /D %installtemp%
 ::Downloads the latest version of Nadeko
 ECHO Downloading Nadeko...
 ECHO.
-git clone -b 1.0 --recursive --depth 1 --progress https://github.com/Kwoth/NadekoBot.git >nul
+git clone -b master --recursive --depth 1 --progress https://github.com/Kwoth/NadekoBot.git >nul
 IF %ERRORLEVEL% EQU 128 (GOTO :giterror)
 TITLE Installing NadekoBot, please wait
 ECHO.
