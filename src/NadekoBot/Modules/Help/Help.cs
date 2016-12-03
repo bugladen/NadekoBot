2a65e5686146cab33ffb2e5739b8bffe4e394170
samvaio <samdivaio@icloud.com>
2016-12-04 00:03:37 +0530

    Update Help.cs

--- src/NadekoBot/Modules/Help/Help.cs
+++ src/NadekoBot/Modules/Help/Help.cs
@@ -109,7 +109,7 @@ namespace NadekoBot.Modules.Help
                 .WithIconUrl(NadekoBot.Client.GetCurrentUser().AvatarUrl))
                 .WithTitle(str)
                 .WithDescription($"{ string.Format(com.Summary, com.Module.Prefix)}{ GetCommandRequirements(com)}")
-                .AddField(fb => fb.WithIndex(1).WithName("**Usage**").WithValue($"{string.Format(com.Remarks, com.Module.Prefix)}").WithIsInline(false)
+                .AddField(fb => fb.WithIndex(1).WithName("**Usage**").WithValue($"{string.Format(com.Remarks, com.Module.Prefix)}").WithIsInline(true)
                 .WithIndex(2).WithName("**TEST**").WithValue($"Testing").WithIsInline(true))
                 .WithColor(NadekoBot.OkColor);
             if (com != null)
