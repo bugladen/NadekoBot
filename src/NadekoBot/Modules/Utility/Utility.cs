b9045e916385e137802c97d2ca03a22e32faff9f
samvaio <samdivaio@icloud.com>
2016-12-03 19:27:45 +0530

    Update Utility.cs

--- src/NadekoBot/Modules/Utility/Utility.cs
+++ src/NadekoBot/Modules/Utility/Utility.cs
@@ -222,7 +222,7 @@ namespace NadekoBot.Modules.Utility
                         },
 
                     },
-                    Color = 0x00bbd6
+                    Color = 0x00bbd6, 0xA8F307
                 });
         }
 
