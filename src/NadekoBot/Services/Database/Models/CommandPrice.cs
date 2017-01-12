70bbecbc8fb475cdcf7026dc4b2c229ded009f73
Kwoth <srki0509@gmail.com>
2017-01-12 23:49:17 +0100

    Guild owners can now run permission commands without permission role

--- src/NadekoBot/Services/Database/Models/CommandPrice.cs
+++ src/NadekoBot/Services/Database/Models/CommandPrice.cs
@@ -9,6 +9,20 @@ namespace NadekoBot.Services.Database.Models
     public class CommandPrice : DbEntity
     {
         public int Price { get; set; }
+        //this is unique
         public string CommandName { get; set; }
+
+        public override int GetHashCode() => 
+            CommandName.GetHashCode();
+
+        public override bool Equals(object obj)
+        {
+            var instance = obj as CommandPrice;
+
+            if (instance == null)
+                return false;
+
+            return instance.CommandName == CommandName;
+        }
     }
 }
