09691adcc69afea3b88401b2e6aa3b46c80b4d57
Kwoth <srki0509@gmail.com>
2017-01-12 20:26:55 +0100

    Woopsie

---
+++ src/NadekoBot/Services/Database/Models/CommandPrice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Services.Database.Models
+{
+    public class CommandPrice : DbEntity
+    {
+        public int Price { get; set; }
+        public string CommandName { get; set; }
+    }
+}
