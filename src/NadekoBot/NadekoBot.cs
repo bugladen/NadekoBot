35d2ada373cfdeee05187841ab28b3d6dc13653c
Kwoth <srki0509@gmail.com>
2017-01-16 15:25:50 +0100

    Don't download users. It breaks the bot for some people?!?!??!

--- src/NadekoBot/NadekoBot.cs
+++ src/NadekoBot/NadekoBot.cs
@@ -96,7 +96,7 @@ namespace NadekoBot
             //connect
             await Client.LoginAsync(TokenType.Bot, Credentials.Token).ConfigureAwait(false);
             await Client.ConnectAsync().ConfigureAwait(false);
-            await Client.DownloadAllUsersAsync().ConfigureAwait(false);
+            //await Client.DownloadAllUsersAsync().ConfigureAwait(false);
             Stats.Initialize();
 
             _log.Info("Connected");
