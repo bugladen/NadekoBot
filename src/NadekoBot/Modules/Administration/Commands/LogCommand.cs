42f7cc9a315f72566379d3c74ac63fc185e7e96c
Kwoth <srki0509@gmail.com>
2017-01-16 14:15:24 +0100

    Fixed empty log spam

--- src/NadekoBot/Modules/Administration/Commands/LogCommand.cs
+++ src/NadekoBot/Modules/Administration/Commands/LogCommand.cs
@@ -397,6 +397,8 @@ namespace NadekoBot.Modules.Administration
                             .AddField(efb => efb.WithName("Old Topic").WithValue(beforeTextChannel.Topic))
                             .AddField(efb => efb.WithName("New Topic").WithValue(afterTextChannel.Topic));
                     }
+                    else
+                        return;
 
                     await logChannel.EmbedAsync(embed).ConfigureAwait(false);
                 }
