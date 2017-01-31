28566023c2661995dea6f83d0c68b52a601a177a
Kwoth <srki0509@gmail.com>
2017-01-31 20:11:42 +0100

    Fixed music errors?

--- src/NadekoBot/Modules/Music/Classes/SongHandler.cs
+++ src/NadekoBot/Modules/Music/Classes/SongHandler.cs
@@ -97,7 +97,7 @@ namespace NadekoBot.Modules.Music.Classes
                 {
                     Title = video.Title.Substring(0, video.Title.Length - 10), // removing trailing "- You Tube"
                     Provider = "YouTube",
-                    Uri = video.Uri,
+                    Uri = await video.GetUriAsync().ConfigureAwait(false),
                     Query = link,
                     ProviderType = musicType,
                 });
