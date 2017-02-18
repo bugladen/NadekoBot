90fa989db0ec6e7b966591952c259eb2d4fadc49
Kwoth <srki0509@gmail.com>
2017-02-18 14:06:58 +0100

    Removed unneeded mentions from >hangman

--- src/NadekoBot/Modules/Games/Commands/Hangman/HangmanGame.cs
+++ src/NadekoBot/Modules/Games/Commands/Hangman/HangmanGame.cs
@@ -145,7 +145,7 @@ namespace NadekoBot.Modules.Games.Commands.Hangman
                         MessagesSinceLastPost = 0;
                         ++Errors;
                         if (Errors < MaxErrors)
-                            await GameChannel.SendErrorAsync("Hangman Game", $"{msg.Author.Mention} Letter `{guess}` has already been used.\n" + ScrambledWord + "\n" + GetHangman(),
+                            await GameChannel.SendErrorAsync("Hangman Game", $"{msg.Author} Letter `{guess}` has already been used.\n" + ScrambledWord + "\n" + GetHangman(),
                                 footer: string.Join(" ", Guesses)).ConfigureAwait(false);
                         else
                             await End().ConfigureAwait(false);
@@ -158,7 +158,7 @@ namespace NadekoBot.Modules.Games.Commands.Hangman
                     {
                         if (GuessedAll)
                         {
-                            try { await GameChannel.SendConfirmAsync("Hangman Game", $"{msg.Author.Mention} guessed a letter `{guess}`!").ConfigureAwait(false); } catch { }
+                            try { await GameChannel.SendConfirmAsync("Hangman Game", $"{msg.Author} guessed a letter `{guess}`!").ConfigureAwait(false); } catch { }
 
                             await End().ConfigureAwait(false);
                             return;
@@ -166,7 +166,7 @@ namespace NadekoBot.Modules.Games.Commands.Hangman
                         MessagesSinceLastPost = 0;
                         try
                         {
-                            await GameChannel.SendConfirmAsync("Hangman Game", $"{msg.Author.Mention} guessed a letter `{guess}`!\n" + ScrambledWord + "\n" + GetHangman(),
+                            await GameChannel.SendConfirmAsync("Hangman Game", $"{msg.Author} guessed a letter `{guess}`!\n" + ScrambledWord + "\n" + GetHangman(),
                           footer: string.Join(" ", Guesses)).ConfigureAwait(false);
                         }
                         catch { }
@@ -177,7 +177,7 @@ namespace NadekoBot.Modules.Games.Commands.Hangman
                         MessagesSinceLastPost = 0;
                         ++Errors;
                         if (Errors < MaxErrors)
-                            await GameChannel.SendErrorAsync("Hangman Game", $"{msg.Author.Mention} Letter `{guess}` does not exist.\n" + ScrambledWord + "\n" + GetHangman(),
+                            await GameChannel.SendErrorAsync("Hangman Game", $"{msg.Author} Letter `{guess}` does not exist.\n" + ScrambledWord + "\n" + GetHangman(),
                                 footer: string.Join(" ", Guesses)).ConfigureAwait(false);
                         else
                             await End().ConfigureAwait(false);
