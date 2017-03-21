dde37f4be816633e23d03dd179d00a98440dd16a
Kwoth <srki0509@gmail.com>
2017-03-21 04:59:25 +0100

    Updated version

--- src/NadekoBot/Services/Impl/StatsService.cs
+++ src/NadekoBot/Services/Impl/StatsService.cs
@@ -16,7 +16,7 @@ namespace NadekoBot.Services.Impl
         private readonly DiscordShardedClient _client;
         private readonly DateTime _started;
 
-        public const string BotVersion = "1.23";
+        public const string BotVersion = "1.25";
 
         public string Author => "Kwoth#2560";
         public string Library => "Discord.Net";
