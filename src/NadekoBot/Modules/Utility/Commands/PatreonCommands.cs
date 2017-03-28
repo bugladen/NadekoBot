ec24ac38864b78faa6340bde78f29524667c4cbf
Kwoth <srki0509@gmail.com>
2017-03-28 08:34:35 +0200

    Added a big part of basic patreon stuff, but can't activate it yet, waiting for their email reply to check some things

--- src/NadekoBot/Modules/Utility/Commands/PatreonCommands.cs
+++ src/NadekoBot/Modules/Utility/Commands/PatreonCommands.cs
@@ -1,78 +1,110 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Discord.Commands;
-using Discord;
-using NadekoBot.Attributes;
-using NadekoBot.Modules.Utility.Models;
-using Newtonsoft.Json;
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Threading.Tasks;
+//using Discord.Commands;
+//using NadekoBot.Attributes;
+//using NadekoBot.Modules.Utility.Models;
+//using Newtonsoft.Json;
+//using System.Threading;
+//using System;
+//using System.Collections.Immutable;
 
-namespace NadekoBot.Modules.Utility
-{
-    public partial class Utility
-    {
-        //[Group]
-        //public class PatreonCommands : NadekoSubmodule
-        //{
-        //    [NadekoCommand, Usage, Description, Aliases]
-        //    [RequireContext(ContextType.Guild)]
-        //    public async Task ClaimPatreonRewards([Remainder] string arg)
-        //    {
-        //        var pledges = await GetPledges2();
-        //    }
+//namespace NadekoBot.Modules.Utility
+//{
+//    public partial class Utility
+//    {
+//        [Group]
+//        public class PatreonCommands : NadekoSubmodule
+//        {
+//            [NadekoCommand, Usage, Description, Aliases]
+//            public async Task ClaimPatreonRewards()
+//            {
+//                var patreon = PatreonThingy.Instance;
 
-        //    private static async Task<Pledge[]> GetPledges()
-        //    {
-        //        var pledges = new List<Pledge>();
-        //        using (var http = new HttpClient())
-        //        {
-        //            http.DefaultRequestHeaders.Clear();
-        //            http.DefaultRequestHeaders.Add("Authorization", "Bearer " + NadekoBot.Credentials.PatreonAccessToken);
-        //            var data = new PatreonData()
-        //            {
-        //                Links = new Links()
-        //                {
-        //                    Next = "https://api.patreon.com/oauth2/api/campaigns/334038/pledges"
-        //                }
-        //            };
-        //            do
-        //            {
-        //                var res =
-        //                    await http.GetStringAsync(data.Links.Next)
-        //                        .ConfigureAwait(false);
-        //                data = JsonConvert.DeserializeObject<PatreonData>(res);
-        //                pledges.AddRange(data.Data);
-        //            } while (!string.IsNullOrWhiteSpace(data.Links.Next));
-        //        }
-        //        return pledges.Where(x => string.IsNullOrWhiteSpace(x.Attributes.declined_since)).ToArray();
-        //    }
+//                var pledges = (await patreon.GetPledges().ConfigureAwait(false))
+//                    .OrderByDescending(x => x.Reward.attributes.amount_cents);
 
-        //    private static async Task<Pledge[]> GetPledges2()
-        //    {
-        //        var pledges = new List<Pledge>();
-        //        using (var http = new HttpClient())
-        //        {
-        //            http.DefaultRequestHeaders.Clear();
-        //            http.DefaultRequestHeaders.Add("Authorization", "Bearer " + NadekoBot.Credentials.PatreonAccessToken);
-        //            var data = new PatreonData()
-        //            {
-        //                Links = new Links()
-        //                {
-        //                    Next = "https://api.patreon.com/oauth2/api/current_user/campaigns?include=pledges"
-        //                }
-        //            };
-        //            do
-        //            {
-        //                var res =
-        //                    await http.GetStringAsync(data.Links.Next)
-        //                        .ConfigureAwait(false);
-        //                data = JsonConvert.DeserializeObject<PatreonData>(res);
-        //                pledges.AddRange(data.Data);
-        //            } while (!string.IsNullOrWhiteSpace(data.Links.Next));
-        //        }
-        //        return pledges.Where(x => string.IsNullOrWhiteSpace(x.Attributes.declined_since)).ToArray();
-        //    }
-        //}
-    }
-}
+//                if (pledges == null)
+//                {
+//                    await ReplyErrorLocalized("pledges_loading").ConfigureAwait(false);
+//                    return;
+//                }
+
+//            }
+//        }
+
+//        public class PatreonThingy
+//        {
+//            public static PatreonThingy _instance = new PatreonThingy();
+//            public static PatreonThingy Instance => _instance;
+
+//            private readonly SemaphoreSlim getPledgesLocker = new SemaphoreSlim(1, 1);
+
+//            private ImmutableArray<PatreonUserAndReward> pledges;
+
+//            static PatreonThingy() { }
+
+//            public async Task<ImmutableArray<PatreonUserAndReward>> GetPledges()
+//            {
+//                try
+//                {
+//                    await LoadPledges().ConfigureAwait(false);
+//                    return pledges;
+//                }
+//                catch (OperationCanceledException)
+//                {
+//                    return pledges;
+//                }
+//            }
+
+//            public async Task LoadPledges()
+//            {
+//                await getPledgesLocker.WaitAsync(1000).ConfigureAwait(false);
+//                try
+//                {
+//                    var rewards = new List<PatreonPledge>();
+//                    var users = new List<PatreonUser>();
+//                    using (var http = new HttpClient())
+//                    {
+//                        http.DefaultRequestHeaders.Clear();
+//                        http.DefaultRequestHeaders.Add("Authorization", "Bearer " + NadekoBot.Credentials.PatreonAccessToken);
+//                        var data = new PatreonData()
+//                        {
+//                            Links = new PatreonDataLinks()
+//                            {
+//                                next = "https://api.patreon.com/oauth2/api/campaigns/334038/pledges"
+//                            }
+//                        };
+//                        do
+//                        {
+//                            var res = await http.GetStringAsync(data.Links.next)
+//                                .ConfigureAwait(false);
+//                            data = JsonConvert.DeserializeObject<PatreonData>(res);
+//                            var pledgers = data.Data.Where(x => x["type"].ToString() == "pledge");
+//                            rewards.AddRange(pledgers.Select(x => JsonConvert.DeserializeObject<PatreonPledge>(x.ToString()))
+//                                .Where(x => x.attributes.declined_since == null));
+//                            users.AddRange(data.Included
+//                                .Where(x => x["type"].ToString() == "user")
+//                                .Select(x => JsonConvert.DeserializeObject<PatreonUser>(x.ToString())));
+//                        } while (!string.IsNullOrWhiteSpace(data.Links.next));
+//                    }
+//                    pledges = rewards.Join(users, (r) => r.relationships?.patron?.data?.id, (u) => u.id, (x, y) => new PatreonUserAndReward()
+//                    {
+//                        User = y,
+//                        Reward = x,
+//                    }).ToImmutableArray();
+//                }
+//                finally
+//                {
+//                    var _ = Task.Run(async () =>
+//                    {
+//                        await Task.Delay(TimeSpan.FromMinutes(5)).ConfigureAwait(false);
+//                        getPledgesLocker.Release();
+//                    });
+                    
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ src/NadekoBot/Services/Database/Models/PatreonRewards.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Services.Database.Models
+{
+    public class PatreonRewards : DbEntity
+    {
+        public ulong UserId { get; set; }
+        public ulong PledgeCents { get; set; }
+        public ulong Awarded { get; set; }
+    }
+}
