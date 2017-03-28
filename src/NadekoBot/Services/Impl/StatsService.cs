e982b0fc1895ffbeb5fa465e77e20c06946728c3
Kwoth <srki0509@gmail.com>
2017-03-28 10:56:51 +0200

    forgot to up the version

--- src/NadekoBot/Services/Impl/StatsService.cs
+++ src/NadekoBot/Services/Impl/StatsService.cs
@@ -16,7 +16,7 @@ namespace NadekoBot.Services.Impl
         private readonly DiscordShardedClient _client;
         private readonly DateTime _started;
 
-        public const string BotVersion = "1.26";
+        public const string BotVersion = "1.26a";
 
         public string Author => "Kwoth#2560";
         public string Library => "Discord.Net";
