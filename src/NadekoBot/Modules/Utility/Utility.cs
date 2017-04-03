d0380b0cbfc2ce45b5f4b0dcc07e32e198b3603e
Kwoth <srki0509@gmail.com>
2017-04-03 12:43:59 +0200

    .ping command added

--- src/NadekoBot/Modules/Utility/Utility.cs
+++ src/NadekoBot/Modules/Utility/Utility.cs
@@ -16,6 +16,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using Discord.WebSocket;
 using NadekoBot.Services;
+using System.Diagnostics;
 
 namespace NadekoBot.Modules.Utility
 {
@@ -493,6 +494,17 @@ namespace NadekoBot.Modules.Utility
                 });
             await Context.User.SendFileAsync(
                 await JsonConvert.SerializeObject(grouping, Formatting.Indented).ToStream().ConfigureAwait(false), title, title).ConfigureAwait(false);
+        }
+        [NadekoCommand, Usage, Description, Aliases]
+        [RequireContext(ContextType.Guild)]
+        public async Task Ping()
+        {
+            var sw = Stopwatch.StartNew();
+            var msg = await Context.Channel.SendMessageAsync("🏓").ConfigureAwait(false);
+            sw.Stop();
+            msg.DeleteAfter(0);
+
+            await Context.Channel.SendConfirmAsync($"{Format.Bold(Context.User.ToString())} 🏓 {(int)sw.Elapsed.TotalMilliseconds}ms").ConfigureAwait(false);
         }
     }
 }
