6a0a749c6776d2fe209af3f237ff00d7d7bff405
Master Kwoth <nadekodiscordbot@gmail.com>
2017-06-10 23:51:22 +0200

    Version changed to 1.41, scripts removed

--- scripts/NadekoRun.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-@TITLE NadekoBot
-CD /D "%~dp0NadekoBot\src\NadekoBot"
-dotnet run --configuration Release
-ECHO NadekoBot has been succesfully stopped, press any key to close this window.
-TITLE NadekoBot - Stopped
-CD /D "%~dp0"
-PAUSE >nul 2>&1
-del NadekoRunNormal.bat
