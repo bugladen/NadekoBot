da85372faeb085e12f630266e21d7895f7bb2c9e
Master Kwoth <nadekodiscordbot@gmail.com>
2017-06-10 15:14:00 +0200

    fixed last commit

--- release.ps1
+++ release.ps1
@@ -9,7 +9,8 @@ function GitHub-Release($versionNumber)
     $lastTag = git describe --tags --abbrev=0
     $tag = "$lastTag..HEAD"
 
-    $clArr = [array]::Reverse((& 'git' 'log', $tag, '--oneline'))
+    $clArr = (& 'git' 'log', $tag, '--oneline')
+    $clArr = [array]::Reverse($clArr)
     $changelog = $clArr | where { "$_" -notlike "*(POEditor.com)*" -and "$_" -notlike "*Merge branch*" -and "$_" -notlike "*Merge pull request*" -and "$_" -notlike "^-*" -and "$_" -notlike "*Merge remote tracking*" }
     $changelog = [string]::join([Environment]::NewLine, $changelog)
 
