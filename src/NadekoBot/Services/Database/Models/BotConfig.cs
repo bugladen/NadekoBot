b0af0fbb083a52a456681efc103f971d313eea15
Master Kwoth <nadekodiscordbot@gmail.com>
2017-06-13 02:50:01 +0200

    Using .h on non-existant command will once again show an error.

--- src/NadekoBot/Services/Database/Models/BotConfig.cs
+++ src/NadekoBot/Services/Database/Models/BotConfig.cs
@@ -42,12 +42,12 @@ namespace NadekoBot.Services.Database.Models
         public HashSet<EightBallResponse> EightBallResponses { get; set; } = new HashSet<EightBallResponse>();
         public HashSet<RaceAnimal> RaceAnimals { get; set; } = new HashSet<RaceAnimal>();
 
-        public string DMHelpString { get; set; } = "Type `-h` for help.";
+        public string DMHelpString { get; set; } = "Type `.h` for help.";
         public string HelpString { get; set; } = @"To add me to your server, use this link -> <https://discordapp.com/oauth2/authorize?client_id={0}&scope=bot&permissions=66186303>
 You can use `{1}modules` command to see a list of all modules.
 You can use `{1}commands ModuleName`
 (for example `{1}commands Administration`) to see a list of all of the commands in that module.
-For a specific command help, use `{1}h CommandName` (for example {1}h !!q)
+For a specific command help, use `{1}h CommandName` (for example {1}h {1}q)
 
 
 **LIST OF COMMANDS CAN BE FOUND ON THIS LINK**
