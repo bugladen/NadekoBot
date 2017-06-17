77754c7deceeed97169dcbf96bc1833d335b99d0
Master Kwoth <nadekodiscordbot@gmail.com>
2017-06-17 13:54:21 +0200

    woops

--- src/NadekoBot/Services/Administration/PruneService.cs
+++ src/NadekoBot/Services/Administration/PruneService.cs
@@ -28,7 +28,7 @@ namespace NadekoBot.Services.Administration
             {
                 IMessage[] msgs;
                 IMessage lastMessage = null;
-                msgs = (await channel.GetMessagesAsync(amount > 100 ? 100 : amount).Flatten()).Where(predicate).ToArray();
+                msgs = (await channel.GetMessagesAsync().Flatten()).Where(predicate).Take(amount).ToArray();
                 while (amount > 0 && msgs.Any())
                 {
                     lastMessage = msgs[msgs.Length - 1];
@@ -50,14 +50,16 @@ namespace NadekoBot.Services.Administration
                     foreach (var group in singleDeletable.GroupBy(x => ++i / (singleDeletable.Count / 5)))
                         await Task.WhenAll(Task.Delay(1000), Task.WhenAll(group.Select(x => x.DeleteAsync()))).ConfigureAwait(false);
 
+                    //this isn't good, because this still work as if i want to remove only specific user's messages from the last
+                    //100 messages, Maybe this needs to be reduced by msgs.Length instead of 100
                     amount -= 100;
                     if(amount > 0)
-                        msgs = (await channel.GetMessagesAsync(lastMessage, Direction.Before, amount > 100 ? 100 : amount).Flatten()).Where(predicate).ToArray();
+                        msgs = (await channel.GetMessagesAsync(lastMessage, Direction.Before).Flatten()).Where(predicate).Take(amount).ToArray();
                 }
             }
-            catch (Exception ex)
+            catch
             {
-
+                //ignore
             }
             finally
             {
