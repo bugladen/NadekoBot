5d136ffd2c0e505afaf56ba307bf8036aa7afca5
Master Kwoth <nadekodiscordbot@gmail.com>
2017-06-20 17:14:55 +0200

    need this to get proper timings on command executions

--- src/NadekoBot/NadekoBot.cs
+++ src/NadekoBot/NadekoBot.cs
@@ -92,7 +92,7 @@ namespace NadekoBot
             CommandService = new CommandService(new CommandServiceConfig()
             {
                 CaseSensitiveCommands = false,
-                DefaultRunMode = RunMode.Async,
+                DefaultRunMode = RunMode.Sync,
             });
 
             //foundation services
