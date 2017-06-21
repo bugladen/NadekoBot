85e183999141682cca9ed773ae5ac08688f10367
Master Kwoth <nadekodiscordbot@gmail.com>
2017-06-21 23:31:47 +0200

    don't load owner channels

--- src/NadekoBot/Services/Administration/SelfService.cs
+++ src/NadekoBot/Services/Administration/SelfService.cs
@@ -67,9 +67,8 @@ namespace NadekoBot.Services.Administration
 
                 _client.Guilds.SelectMany(g => g.Users);
 
-                LoadOwnerChannels();
-
-                
+                //todo load owner channels
+                //LoadOwnerChannels();                
             });
         }
 
