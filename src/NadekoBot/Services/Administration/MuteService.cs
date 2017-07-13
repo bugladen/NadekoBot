390a637c08bda4a5b990bf1c4e3380b896a73380
hakufu <hakufusdragon@gmail.com>
2017-07-13 14:08:51 -0400

    Add deny perm for addReactions on mute
    Added the permission for the channel override when muting to also include the denial of "addReactions."

--- src/NadekoBot/Services/Administration/MuteService.cs
+++ src/NadekoBot/Services/Administration/MuteService.cs
@@ -30,7 +30,7 @@ namespace NadekoBot.Services.Administration
         public event Action<IGuildUser, MuteType> UserMuted = delegate { };
         public event Action<IGuildUser, MuteType> UserUnmuted = delegate { };
 
-        private static readonly OverwritePermissions denyOverwrite = new OverwritePermissions(sendMessages: PermValue.Deny, attachFiles: PermValue.Deny);
+        private static readonly OverwritePermissions denyOverwrite = new OverwritePermissions(addReactions: PermValue.Deny, sendMessages: PermValue.Deny, attachFiles: PermValue.Deny);
 
         private readonly Logger _log = LogManager.GetCurrentClassLogger();
         private readonly DiscordSocketClient _client;
