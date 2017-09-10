268f9b0448df419d07cc9a1c319a8dbfa30dd05f
Master Kwoth <nadekodiscordbot@gmail.com>
2017-09-10 17:58:32 +0200

    fixed .xpglb help string. Version upped to 1.8.1

--- src/NadekoBot/Services/Impl/StatsService.cs
+++ src/NadekoBot/Services/Impl/StatsService.cs
@@ -20,7 +20,7 @@ namespace NadekoBot.Services.Impl
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "1.8";
+        public const string BotVersion = "1.8.1";
 
         public string Author => "Kwoth#2560";
         public string Library => "Discord.Net";
