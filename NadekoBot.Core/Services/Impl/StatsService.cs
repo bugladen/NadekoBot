fe4cfa732fce07fea3ef164178f23e285d770f2f
Master Kwoth <nadekodiscordbot@gmail.com>
2017-10-21 12:15:23 +0200

    Version upped to 2.0.0

--- NadekoBot.Core/Services/Impl/StatsService.cs
+++ NadekoBot.Core/Services/Impl/StatsService.cs
@@ -23,7 +23,7 @@ namespace NadekoBot.Core.Services.Impl
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "2.0.0-beta";
+        public const string BotVersion = "2.0.0";
 
         public string Author => "Kwoth#2560";
         public string Library => "Discord.Net";
