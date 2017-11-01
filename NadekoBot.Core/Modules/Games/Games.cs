7beee8df707dac4955cf52fa5a61be474ed68165
Master Kwoth <master.kwoth@gmail.com>
2017-11-01 03:10:44 +0100

    remove trickortreat

--- NadekoBot.Core/Modules/Games/Games.cs
+++ NadekoBot.Core/Modules/Games/Games.cs
@@ -16,6 +16,7 @@ namespace NadekoBot.Modules.Games
     - Shiritori
     - Simple RPG adventure
     */
+    //todo update docs
     public partial class Games : NadekoTopLevelModule<GamesService>
     {
         private readonly IImagesService _images;
@@ -24,35 +25,35 @@ namespace NadekoBot.Modules.Games
         {
             _images = images;
         }
-#if GLOBAL_NADEKO
-        [NadekoCommand, Usage, Description, Aliases]
-        [RequireContext(ContextType.Guild)]
-        public async Task TrickOrTreat()
-        {
-            if (DateTime.UtcNow.Day != 31 ||
-                DateTime.UtcNow.Month != 10
-                || !_service.HalloweenAwardedUsers.Add(Context.User.Id)
-        )
-            {
-                return;
-            }
-            if (await _service.GetTreat(Context.User.Id))
-            {
-                await Context.Channel
-                    .SendConfirmAsync($"You've got a treat of 10🍬! Happy Halloween!")
-                    .ConfigureAwait(false);
-            }
-            else
-            {
-                await Context.Channel
-                    .EmbedAsync(new EmbedBuilder()
-                    .WithDescription("No treat for you :c Happy Halloween!")
-                    .WithImageUrl("http://tinyurl.com/ybntddbb")
-                    .WithErrorColor())
-                    .ConfigureAwait(false);
-            }
-        }
-#endif
+//#if GLOBAL_NADEKO
+//        [NadekoCommand, Usage, Description, Aliases]
+//        [RequireContext(ContextType.Guild)]
+//        public async Task TrickOrTreat()
+//        {
+//            if (DateTime.UtcNow.Day != 31 ||
+//                DateTime.UtcNow.Month != 10
+//                || !_service.HalloweenAwardedUsers.Add(Context.User.Id)
+//        )
+//            {
+//                return;
+//            }
+//            if (await _service.GetTreat(Context.User.Id))
+//            {
+//                await Context.Channel
+//                    .SendConfirmAsync($"You've got a treat of 10🍬! Happy Halloween!")
+//                    .ConfigureAwait(false);
+//            }
+//            else
+//            {
+//                await Context.Channel
+//                    .EmbedAsync(new EmbedBuilder()
+//                    .WithDescription("No treat for you :c Happy Halloween!")
+//                    .WithImageUrl("http://tinyurl.com/ybntddbb")
+//                    .WithErrorColor())
+//                    .ConfigureAwait(false);
+//            }
+//        }
+//#endif
         [NadekoCommand, Usage, Description, Aliases]
         public async Task Choose([Remainder] string list = null)
         {
