0f76adbc04a4870f9e15d4a43fa081c532d51b50
Master Kwoth <master.kwoth@gmail.com>
2017-12-26 21:47:03 +0700

    .startevent reaction will now work if you have animated currency emoji

--- NadekoBot.Core/Modules/Utility/Utility.cs
+++ NadekoBot.Core/Modules/Utility/Utility.cs
@@ -70,7 +70,7 @@ namespace NadekoBot.Modules.Utility
             }
             var rng = new NadekoRandom();
             var arr = await Task.Run(() => socketGuild.Users
-                    .Where(u => u.Game?.Name?.ToUpperInvariant() == game)
+                    .Where(u => u.Activity?.Name?.ToUpperInvariant() == game)
                     .Select(u => u.Username)
                     .OrderBy(x => rng.Next())
                     .Take(60)
