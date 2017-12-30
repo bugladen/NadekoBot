8e9fa55569d41ff7fc071baa91d63d9aa43d4c12
Master Kwoth <master.kwoth@gmail.com>
2017-12-30 20:56:15 +0700

    Added .remindlist, .remindrm (you can now list and remove previously set reminders), also added Ukrainian langugae to the list of supported languages

--- NadekoBot.Core/Modules/Utility/RemindCommands.cs
+++ NadekoBot.Core/Modules/Utility/RemindCommands.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using NadekoBot.Common.Attributes;
 using NadekoBot.Modules.Administration.Services;
 using NadekoBot.Modules.Utility.Services;
+using System.Linq;
 
 namespace NadekoBot.Modules.Utility
 {
@@ -56,6 +57,79 @@ namespace NadekoBot.Modules.Utility
                 else
                 {
                     var _ = RemindInternal(channel.Id, false, timeStr, message).ConfigureAwait(false);
+                }
+            }
+
+            [NadekoCommand, Usage, Description, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task RemindList(int page = 1)
+            {
+                if (--page < 0)
+                    return;
+
+                var embed = new EmbedBuilder()
+                    .WithOkColor()
+                    .WithTitle(GetText("reminder_list"));
+
+                List<Reminder> rems;
+                using (var uow = _db.UnitOfWork)
+                {
+                    rems = uow.Reminders.RemindersFor(Context.User.Id, page)
+                        .ToList();
+                }
+
+                if (rems.Any())
+                {
+                    var i = 0;
+                    foreach (var rem in rems)
+                    {
+                        var when = rem.When;
+                        var diff = when - DateTime.UtcNow;
+                        embed.AddField($"#{++i} {rem.When:HH:mm yyyy-MM-dd} UTC (in {(int)diff.TotalHours}h {(int)diff.Minutes}m)", $@"`Target:` {(rem.IsPrivate ? "DM" : "Channel")}
+`TargetId:` {rem.ChannelId}
+`Message:` {rem.Message}", false);
+                    }
+                }
+                else
+                {
+                    embed.WithDescription(GetText("reminders_none"));
+                }
+
+                embed.AddPaginatedFooter(page + 1, null);
+                await Context.Channel.EmbedAsync(embed).ConfigureAwait(false);
+            }
+
+            [NadekoCommand, Usage, Description, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task RemindDelete(int index)
+            {
+                if (--index < 0)
+                    return;
+
+                var embed = new EmbedBuilder();
+
+                Reminder rem = null;
+                using (var uow = _db.UnitOfWork)
+                {
+                    var rems = uow.Reminders.RemindersFor(Context.User.Id, index / 10)
+                        .ToList();
+
+                    if (rems.Count > index)
+                    {
+                        rem = rems[index];
+                        uow.Reminders.Remove(rem);
+                        uow.Complete();
+                    }
+                }
+
+                if (rem == null)
+                {
+                    await ReplyErrorLocalized("reminder_not_exist").ConfigureAwait(false);
+                }
+                else
+                {
+                    _service.RemoveReminder(rem.Id);
+                    await ReplyErrorLocalized("reminder_deleted", index + 1).ConfigureAwait(false);
                 }
             }
 
@@ -133,7 +207,7 @@ namespace NadekoBot.Modules.Utility
                 {
                     // ignored
                 }
-                await _service.StartReminder(rem);
+                _service.StartReminder(rem);
             }
             
             [NadekoCommand, Usage, Description, Aliases]
