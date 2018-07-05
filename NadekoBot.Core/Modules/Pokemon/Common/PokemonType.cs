87830ffc9f3be7f8c4f66637d814671c7cf88b52
Master Kwoth <master.kwoth@gmail.com>
2018-07-06 04:20:22 +0700

    .attack works again, fixes #2396

--- NadekoBot.Core/Modules/Pokemon/Common/PokemonType.cs
+++ NadekoBot.Core/Modules/Pokemon/Common/PokemonType.cs
@@ -4,15 +4,8 @@ namespace NadekoBot.Modules.Pokemon.Common
 {
     public class PokemonType
     {
-        public PokemonType(string n, string i, string[] m, List<PokemonMultiplier> multi)
-        {
-            Name = n;
-            Icon = i;
-            Moves = m;
-            Multipliers = multi;
-        }
         public string Name { get; set; }
-        public List<PokemonMultiplier> Multipliers { get; }
+        public List<PokemonMultiplier> Multipliers { get; set;  }
         public string Icon { get; set; }
         public string[] Moves { get; set; }
 
@@ -21,11 +14,6 @@ namespace NadekoBot.Modules.Pokemon.Common
     }
     public class PokemonMultiplier
     {
-        public PokemonMultiplier(string t, double m)
-        {
-            Type = t;
-            Multiplication = m;
-        }
         public string Type { get; set; }
         public double Multiplication { get; set; }
     }
