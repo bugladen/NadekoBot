f06cfd7b511a2e83fef52ed195a03aed0afdf77d
Master Kwoth <master.kwoth@gmail.com>
2018-08-02 16:39:14 +0700

    Invite commands refined. .crinv renamed to .invitecreate, added .invitedelete and .invitelist. .invcr also takes some settings now

---
+++ NadekoBot.Core/Modules/Utility/Services/InviteService.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using NadekoBot.Core.Common;
+using NadekoBot.Core.Services;
+
+namespace NadekoBot.Core.Modules.Utility.Services
+{
+    public class InviteService : INService
+    {
+        public class Options : INadekoCommandOptions
+        {
+            [Option('m', "max-uses", Required = false, Default = 0, HelpText = "Maximum number of times the invite can be used. Default 0 (never).")]
+            public int MaxUses { get; set; } = 0;
+
+            [Option('u', "unique", Required = false, Default = false, HelpText = "Not setting this flag will result in bot getting the existing invite with the same settings if it exists, instead of creating a new one.")]
+            public bool Unique { get; set; } = false;
+
+            public void NormalizeOptions()
+            {
+                if (MaxUses < 0)
+                    MaxUses = 0;
+            }
+        }
+    }
+}
